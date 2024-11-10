81015f58f727add7bdff51287cc2c8f13c78498b
claudio silva <claudiosilva.one@gmail.com>
2024-11-10 20:08:20 -0300

    chore: add CORS configuration

--- src/middleware/corsConfig.ts
+++ src/middleware/corsConfig.ts
@@ -1,15 +1,9 @@
-import cors from "cors";
-
-if (!process.env.NODE_ENV || !process.env.URL_DOMAIN) {
-  throw new Error("Environment variable process.env.URL_DOMAIN is not defined");
-}
-
-const domain =
-  process.env.NODE_ENV === "production" ? process.env.URL_DOMAIN : "*";
-
-export const corsOptions = cors({
-  origin: domain,
+import cors, { CorsOptions } from "cors";
+const corsOptions: CorsOptions = {
+  origin: "*",
   methods: "GET,POST,DELETE,PATCH",
   credentials: true,
-  allowedHeaders : ["Content-Type", "Authorization"]
-});
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+export const corsMiddleware = cors(corsOptions);
