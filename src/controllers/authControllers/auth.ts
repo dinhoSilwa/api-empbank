0e2f657cb52b2f2d4a8b4e80bc3acee33dd89455
claudio silva <claudiosilva.one@gmail.com>
2024-11-16 10:41:10 -0300

    feat : class to authenticated jwt Tokens

--- src/controllers/authControllers/auth.ts
+++ src/controllers/authControllers/auth.ts
@@ -1,19 +1,25 @@
 import { Response, Request } from "express";
 import { AuthService } from "../../service/auth/authService";
+import { emit } from "process";
 
 export class AuthController {
   static async create(req: Request, res: Response): Promise<void> {
     try {
       const userAuth = await AuthService.createAuth(req.body);
-      res.status(201).json({ auth: userAuth });
+      const { name, email } = userAuth;
+      res.status(201).json({ name, email });
     } catch (error) {
       console.error("Falha ao Cadastrar", error);
-      res.status(500).json({ msgErro: "Falha ao Cadastrar Usuário" });
+      res.status(500).json({ msgErro: `Falha ao Cadastrar Usuário ${error}` });
     }
   }
 
   static async credentials(req: Request, res: Response): Promise<void> {
-    const userCredentials = await AuthService.credentials(req.body);
-    res.status(200).json({ userCredentials });
+    try {
+      const userCredentials = await AuthService.credentials(req.body);
+      res.status(200).json({ userCredentials });
+    } catch (error) {
+      res.send(404).json({ msgError: `Falha ao Credenciar ${error}` });
+    }
   }
 }
--- src/middleware/authToken.ts
+++ src/middleware/authToken.ts
@@ -1,11 +1,19 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const SECRET = process.env.JWT_SECRET_KEY as string;
 
 export class TokenManager {
   private static instance: TokenManager;
   private secretKey: string;
 
   private constructor() {
-    this.secretKey = process.env.JWT_SECRET_KEY!;
+    this.secretKey = SECRET || "";
+    if (!this.secretKey) {
+      throw new Error("Falha ao obter token");
+    }
   }
 
   public static getInstance(): TokenManager {
@@ -15,25 +23,40 @@ export class TokenManager {
     return TokenManager.instance;
   }
 
-  public generateToken(payload: object, expiresIn: string = "1h"): string {
-    return jwt.sign(payload, this.secretKey, { expiresIn });
-  }
-
-  public decodeToken(token: string): JwtPayload | string | null {
+  public generateToken(
+    payload: object,
+    expiresIn: string = "1h"
+  ): string | null {
     try {
-      return jwt.verify(token, this.secretKey);
+      return jwt.sign(payload, this.secretKey, { expiresIn });
     } catch (error) {
-      console.error("Token validation error", error);
-      return null;
+      if (error instanceof Error) {
+        throw new Error(`Falha gerar Token ${error.message}`);
+      }
+      throw new Error("Falha ao gerar Token , erro desconhecido");
     }
   }
 
-  public decode(token: string): JwtPayload | null {
+  public encodeToken(token: string): JwtPayload | null {
     try {
+      if (!token) return null;
       return jwt.decode(token) as JwtPayload;
     } catch (error) {
-      console.error("Token decoding error", error);
-      return null;
+      if (error instanceof Error)
+        throw new Error(`Falha ao encodificar o Token ${error.message}`);
+    }
+    throw new Error("Falha ao encodificar o Token, erro desconhecido");
+  }
+
+  public verifyToken(token: string): JwtPayload | string | null {
+    try {
+      if (!token) return null;
+      return jwt.verify(token, this.secretKey);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Falha ao verificar o Token ${error.message}`);
+      }
+      throw new Error("Falha ao verificar o Token");
     }
   }
 }
