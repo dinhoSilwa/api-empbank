1b65d6a7be15a6836a6d543a416d5aef84c790a1
claudio silva <claudiosilva.one@gmail.com>
2025-05-10 21:10:22 -0300

    feat: add-middleware-to-generate-and-validate-jwt

---
+++ src/middleware/tokenMiddlerware.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from "express";
+import { TokenManager } from "../token/tokenManager";
+
+export class AuthMiddleware {
+  constructor(private JWT: TokenManager) {
+    this.JWT = TokenManager.getInstance();
+  }
+
+  public createToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void => {
+    const { email, nome } = req.body;
+    const createToken = this.JWT.generateToken({ email, nome });
+    if (
+      !createToken ||
+      createToken.length === 0 ||
+      createToken !== typeof "string"
+    ) {
+      res.status(404).json({ msg: "Acesso Negado , Token Vazio" });
+      return;
+    }
+    try {
+      const decode = this.JWT.encodeToken(createToken);
+      req.user = decode;
+    } catch (err) {
+      res.status(404).json({ msg: "Falha ao Gerar o Token" });
+    }
+  };
+
+  public verifyToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void => {
+    const headers = req.get("Authorization");
+    const token = headers?.split(" ")[1];
+
+    if (!token || token.length === 0 || token !== typeof "string") {
+      res.status(404).json({ msg: "Acesso Negado" });
+      return;
+    }
+
+    try {
+      const verify = this.JWT.verifyToken(token);
+      if (!verify || verify.length === 0 || verify !== typeof "string")
+        res.status(200).json({ msg: "Acesso Autorizado" });
+      next();
+    } catch (erro) {
+      res.status(500).json({ msg: "Acesso Negado, Erro ao solicitar Token" });
+      return;
+    }
+  };
+}
