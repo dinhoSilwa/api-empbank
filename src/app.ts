cef2644a8030c03687eeadb5d77bf7065f60938d
claudio silva <claudiosilva.one@gmail.com>
2025-05-12 15:25:06 -0300

    refactor: make-error-middleware-global-and-remove-try-catch-blocks

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express, { Application } from "express";
+import "express-async-errors";
+import express, { Application, Request, Response } from "express";
 import { corsMiddleware } from "./middleware/corsConfig";
 import { authRouter } from "./routers/authRouters/auth";
+import { ErrorHandler } from "./middleware/errorsMiddleware";
+import { error } from "console";
 export const app: Application = express();
 app.use(corsMiddleware);
 app.use(express.json());
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
+app.get("*", (req: Request, res: Response) => {
+  res.status(500).json({ msg: "Rota não encontrada" });
+});
+app.use(ErrorHandler);
--- src/controllers/authControllers/auth.ts
+++ src/controllers/authControllers/auth.ts
@@ -1,30 +1,21 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import { AuthService } from "../../service/auth/authService";
+import { httpStatus } from "../../utils/httpstatus";
 
 export class AuthController {
-  static async create(req: Request, res: Response): Promise<void> {
-    try {
-      const userAuth = await AuthService.createAuth(req.body);
-      const { name, email } = userAuth;
-      res.status(201).json({ name, email });
-    } catch (error) {
-      console.error("Falha ao Cadastrar", error);
-      res.status(500).json({ msgErro: `Falha ao Cadastrar Usuário` });
-    }
+  static async create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const userAuth = await AuthService.createAuth(req.body);
+    const { name, email } = userAuth;
+    res.status(httpStatus.OK).json({ name, email });
   }
 
   static async credentials(req: Request, res: Response): Promise<void> {
-    try {
-      const token = await AuthService.credentials(req.body);
-
-      if (!token) {
-        res.status(401).json({ msg: "Não Autorizado" });
-        return;
-      }
-      res.setHeader("Authorization", `Bearer ${token}`);
-      res.status(200).json({ msg: "Acesso Liberado", token });
-    } catch (error) {
-      res.status(500).json({ msgError: `Falha ao Credenciar` });
-    }
+    const token = await AuthService.credentials(req.body);
+    res.setHeader("Authorization", `Bearer ${token}`);
+    res.status(httpStatus.OK).json({ msg: "Acesso Liberado", token });
   }
 }
