16615dc5674bdd34dbc2887c5123e36732f80e72
claudio silva <claudiosilva.one@gmail.com>
2025-05-14 15:14:44 -0300

    feat: add-type-and-existence-check

--- src/utils/extractIdFromToken.ts
+++ src/utils/extractIdFromToken.ts
@@ -1,8 +1,10 @@
 import { IncomingHttpHeaders } from "http";
 import { TokenManager } from "../token/tokenManager";
+import { Unauthorized } from "../errors/customsErrors";
 
 export const extractIdFromToken = (headers: IncomingHttpHeaders): string => {
   const token = headers["authorization"]?.split(" ")[1];
   const { id } = TokenManager.getInstance().verifyToken(token!);
+  if (!id) throw new Unauthorized("Falha ao Obter Token");
   return id;
 };
